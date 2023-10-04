e59e53a4c9244c57b1283bee4fe432cbf62eb818
Nyttho <anthony.dms@outlook.com>
2023-10-04 16:22:20 +0200

    inscription réussite

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const bodyParser = require("body-parser");
+
 const password = require("./utils/password");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 const bookRoutes = require("./routes/books");
 const userRoutes = require("./routes/user");
@@ -13,8 +24,6 @@ mongoose
   )
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch(() => console.log("Connexion à MongoDB échouée !"));
-
-const app = express();
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -29,265 +38,265 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/api/book", bookRoutes);
+app.use("/api/books", bookRoutes);
 app.use("/api/auth", userRoutes);
 
-// app.get("/api/books", (req, res, next) => {
-//   const books = [
-//     {
-//       id: "1",
-//       userId: "clc4wj5lh3gyi0ak4eq4n8syr",
-//       title: "Milwaukee Mission",
-//       author: "Elder Cooper",
-//       imageUrl: "https://via.placeholder.com/206x260",
-//       year: 2021,
-//       genre: "Policier",
-//       ratings: [
-//         {
-//           userId: "1",
-//           grade: 5,
-//         },
-//         {
-//           userId: "1",
-//           grade: 5,
-//         },
-//         {
-//           userId: "clc4wj5lh3gyi0ak4eq4n8syr",
-//           grade: 5,
-//         },
-//         {
-//           userId: "1",
-//           grade: 5,
-//         },
-//       ],
-//       averageRating: 3,
-//     },
-//     {
-//       id: "2",
-//       userId: "clbxs3tag6jkr0biul4trzbrv",
-//       title: "Book for Esther",
-//       author: "Alabaster",
-//       imageUrl: "https://via.placeholder.com/206x260",
-//       year: 2022,
-//       genre: "Paysage",
-//       ratings: [
-//         {
-//           userId: "clbxs3tag6jkr0biul4trzbrv",
-//           grade: 4,
-//         },
-//         {
-//           userId: "1",
-//           grade: 5,
-//         },
-//         {
-//           userId: "1",
-//           grade: 5,
-//         },
-//         {
-//           userId: "1",
-//           grade: 5,
-//         },
-//       ],
-//       averageRating: 4.2,
-//     },
-//     {
-//       id: "3",
-//       userId: "1",
-//       title: "The Kinfolk Table",
-//       author: "Nathan Williams",
-//       imageUrl: "https://via.placeholder.com/206x260",
-//       year: 2022,
-//       genre: "Cuisine",
-//       ratings: [
-//         {
-//           userId: "1",
-//           grade: 5,
-//         },
-//         {
-//           userId: "1",
-//           grade: 5,
-//         },
-//         {
-//           userId: "1",
-//           grade: 5,
-//         },
-//         {
-//           userId: "1",
-//           grade: 5,
-//         },
-//       ],
-//       averageRating: 3,
-//     },
-//     {
-//       id: "4",
-//       userId: "1",
-//       title: "Milwaukee Mission",
-//       author: "Elder Cooper",
-//       imageUrl: "https://via.placeholder.com/206x260",
-//       year: 2021,
-//       genre: "Policier",
-//       ratings: [
-//         {
-//           userId: "1",
-//           grade: 5,
-//         },
-//         {
-//           userId: "1",
-//           grade: 5,
-//         },
-//         {
-//           userId: "1",
-//           grade: 5,
-//         },
-//         {
-//           userId: "1",
-//           grade: 5,
-//         },
-//       ],
-//       averageRating: 3,
-//     },
-//     {
-//       id: "5",
-//       userId: "1",
-//       title: "Book for Esther",
-//       author: "Alabaster",
-//       imageUrl: "https://via.placeholder.com/206x260",
-//       year: 2022,
-//       genre: "Paysage",
-//       ratings: [
-//         {
-//           userId: "1",
-//           grade: 5,
-//         },
-//         {
-//           userId: "1",
-//           grade: 5,
-//         },
-//         {
-//           userId: "1",
-//           grade: 5,
-//         },
-//         {
-//           userId: "1",
-//           grade: 5,
-//         },
-//       ],
-//       averageRating: 4,
-//     },
-//     {
-//       id: "6",
-//       userId: "1",
-//       title: "The Kinfolk Table",
-//       author: "Nathan Williams",
-//       imageUrl: "https://via.placeholder.com/206x260",
-//       year: 2022,
-//       genre: "Cuisine",
-//       ratings: [
-//         {
-//           userId: "1",
-//           grade: 5,
-//         },
-//         {
-//           userId: "1",
-//           grade: 5,
-//         },
-//         {
-//           userId: "1",
-//           grade: 5,
-//         },
-//         {
-//           userId: "1",
-//           grade: 5,
-//         },
-//       ],
-//       averageRating: 3,
-//     },
-//     {
-//       id: "7",
-//       userId: "1",
-//       title: "Milwaukee Mission",
-//       author: "Elder Cooper",
-//       imageUrl: "https://via.placeholder.com/206x260",
-//       year: 2021,
-//       genre: "Policier",
-//       ratings: [
-//         {
-//           userId: "1",
-//           grade: 5,
-//         },
-//         {
-//           userId: "1",
-//           grade: 5,
-//         },
-//         {
-//           userId: "1",
-//           grade: 5,
-//         },
-//         {
-//           userId: "1",
-//           grade: 5,
-//         },
-//       ],
-//       averageRating: 3,
-//     },
-//     {
-//       id: "8",
-//       userId: "clc7s9xnh7zpt0ak4fisdwuj1",
-//       title: "Book for Esther",
-//       author: "Alabaster",
-//       imageUrl: "https://via.placeholder.com/206x260",
-//       year: 2022,
-//       genre: "Paysage",
-//       ratings: [
-//         {
-//           userId: "1",
-//           grade: 5,
-//         },
-//         {
-//           userId: "1",
-//           grade: 5,
-//         },
-//         {
-//           userId: "1",
-//           grade: 5,
-//         },
-//         {
-//           userId: "1",
-//           grade: 5,
-//         },
-//       ],
-//       averageRating: 4,
-//     },
-//     {
-//       id: "9",
-//       userId: "clc4wj5lh3gyi0ak4eq4n8syr",
-//       title: "The Kinfolk Table",
-//       author: "Nathan Williams",
-//       imageUrl: "https://via.placeholder.com/206x260",
-//       year: 2022,
-//       genre: "Cuisine",
-//       ratings: [
-//         {
-//           userId: "1",
-//           grade: 5,
-//         },
-//         {
-//           userId: "1",
-//           grade: 5,
-//         },
-//         {
-//           userId: "1",
-//           grade: 5,
-//         },
-//         {
-//           userId: "clc4wj5lh3gyi0ak4eq4n8syr",
-//           grade: 1,
-//         },
-//       ],
-//       averageRating: 3,
-//     },
-//   ];
-//   res.status(200).json(books);
-// });
+app.get("/api/books", (req, res, next) => {
+  const books = [
+    {
+      id: "1",
+      userId: "clc4wj5lh3gyi0ak4eq4n8syr",
+      title: "Milwaukee Mission",
+      author: "Elder Cooper",
+      imageUrl: "https://via.placeholder.com/206x260",
+      year: 2021,
+      genre: "Policier",
+      ratings: [
+        {
+          userId: "1",
+          grade: 5,
+        },
+        {
+          userId: "1",
+          grade: 5,
+        },
+        {
+          userId: "clc4wj5lh3gyi0ak4eq4n8syr",
+          grade: 5,
+        },
+        {
+          userId: "1",
+          grade: 5,
+        },
+      ],
+      averageRating: 3,
+    },
+    {
+      id: "2",
+      userId: "clbxs3tag6jkr0biul4trzbrv",
+      title: "Book for Esther",
+      author: "Alabaster",
+      imageUrl: "https://via.placeholder.com/206x260",
+      year: 2022,
+      genre: "Paysage",
+      ratings: [
+        {
+          userId: "clbxs3tag6jkr0biul4trzbrv",
+          grade: 4,
+        },
+        {
+          userId: "1",
+          grade: 5,
+        },
+        {
+          userId: "1",
+          grade: 5,
+        },
+        {
+          userId: "1",
+          grade: 5,
+        },
+      ],
+      averageRating: 4.2,
+    },
+    {
+      id: "3",
+      userId: "1",
+      title: "The Kinfolk Table",
+      author: "Nathan Williams",
+      imageUrl: "https://via.placeholder.com/206x260",
+      year: 2022,
+      genre: "Cuisine",
+      ratings: [
+        {
+          userId: "1",
+          grade: 5,
+        },
+        {
+          userId: "1",
+          grade: 5,
+        },
+        {
+          userId: "1",
+          grade: 5,
+        },
+        {
+          userId: "1",
+          grade: 5,
+        },
+      ],
+      averageRating: 3,
+    },
+    {
+      id: "4",
+      userId: "1",
+      title: "Milwaukee Mission",
+      author: "Elder Cooper",
+      imageUrl: "https://via.placeholder.com/206x260",
+      year: 2021,
+      genre: "Policier",
+      ratings: [
+        {
+          userId: "1",
+          grade: 5,
+        },
+        {
+          userId: "1",
+          grade: 5,
+        },
+        {
+          userId: "1",
+          grade: 5,
+        },
+        {
+          userId: "1",
+          grade: 5,
+        },
+      ],
+      averageRating: 3,
+    },
+    {
+      id: "5",
+      userId: "1",
+      title: "Book for Esther",
+      author: "Alabaster",
+      imageUrl: "https://via.placeholder.com/206x260",
+      year: 2022,
+      genre: "Paysage",
+      ratings: [
+        {
+          userId: "1",
+          grade: 5,
+        },
+        {
+          userId: "1",
+          grade: 5,
+        },
+        {
+          userId: "1",
+          grade: 5,
+        },
+        {
+          userId: "1",
+          grade: 5,
+        },
+      ],
+      averageRating: 4,
+    },
+    {
+      id: "6",
+      userId: "1",
+      title: "The Kinfolk Table",
+      author: "Nathan Williams",
+      imageUrl: "https://via.placeholder.com/206x260",
+      year: 2022,
+      genre: "Cuisine",
+      ratings: [
+        {
+          userId: "1",
+          grade: 5,
+        },
+        {
+          userId: "1",
+          grade: 5,
+        },
+        {
+          userId: "1",
+          grade: 5,
+        },
+        {
+          userId: "1",
+          grade: 5,
+        },
+      ],
+      averageRating: 3,
+    },
+    {
+      id: "7",
+      userId: "1",
+      title: "Milwaukee Mission",
+      author: "Elder Cooper",
+      imageUrl: "https://via.placeholder.com/206x260",
+      year: 2021,
+      genre: "Policier",
+      ratings: [
+        {
+          userId: "1",
+          grade: 5,
+        },
+        {
+          userId: "1",
+          grade: 5,
+        },
+        {
+          userId: "1",
+          grade: 5,
+        },
+        {
+          userId: "1",
+          grade: 5,
+        },
+      ],
+      averageRating: 3,
+    },
+    {
+      id: "8",
+      userId: "clc7s9xnh7zpt0ak4fisdwuj1",
+      title: "Book for Esther",
+      author: "Alabaster",
+      imageUrl: "https://via.placeholder.com/206x260",
+      year: 2022,
+      genre: "Paysage",
+      ratings: [
+        {
+          userId: "1",
+          grade: 5,
+        },
+        {
+          userId: "1",
+          grade: 5,
+        },
+        {
+          userId: "1",
+          grade: 5,
+        },
+        {
+          userId: "1",
+          grade: 5,
+        },
+      ],
+      averageRating: 4,
+    },
+    {
+      id: "9",
+      userId: "clc4wj5lh3gyi0ak4eq4n8syr",
+      title: "The Kinfolk Table",
+      author: "Nathan Williams",
+      imageUrl: "https://via.placeholder.com/206x260",
+      year: 2022,
+      genre: "Cuisine",
+      ratings: [
+        {
+          userId: "1",
+          grade: 5,
+        },
+        {
+          userId: "1",
+          grade: 5,
+        },
+        {
+          userId: "1",
+          grade: 5,
+        },
+        {
+          userId: "clc4wj5lh3gyi0ak4eq4n8syr",
+          grade: 1,
+        },
+      ],
+      averageRating: 3,
+    },
+  ];
+  res.status(200).json(books);
+});
 
 module.exports = app;
